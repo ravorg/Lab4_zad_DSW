2ce64fa76284b4a8cc292c4334c31a6c3220670c
ravorg <33811749+ravorg@users.noreply.github.com>
2023-05-22 14:48:48 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-ARGS=$(getopt -a --options d --long "data" -- "$@")
+ARGS=$(getopt -a --options d --long "date" -- "$@")
 eval set -- "$ARGS"
 date="false"
 while true; do
   case "$1" in
-    --data)
+    --date)
         echo $(date)
       date="true"
       shift;;
     --)
       break;;
      *)
-      printf "Błędna opcja %s\nDostępne opcje: [-d --data Data]" "$1"
+      printf "Błędna opcja %s\nDostępne opcje: [--date Data]" "$1"
       exit 1;;
   esac
 done
